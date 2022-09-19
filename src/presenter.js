14c7a544451ca563bbe8ee55fefcf97a68a62171
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2022-09-19 15:55:23 -0400

    Implementacion de interfaz para interactuar con el usuario

--- src/presenter.js
+++ src/presenter.js
@@ -1,30 +1,34 @@
 import sumar from "./sumador";
 import multiplicar from "./multiplicador";
+import CalculadoraCadena from "./calculadoraCadena";
+
+const cd= new CalculadoraCadena();
 
 const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
+//const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
-const firstMul = document.querySelector("#primer-numeroMul");
-const secondMul = document.querySelector("#segundo-numeroMul");
-const formMul =document.querySelector("#multiplicar-form");
-const divMul= document.querySelector("#resultado-divMul");
+//const firstMul = document.querySelector("#primer-numeroMul");
+//const secondMul = document.querySelector("#segundo-numeroMul");
+//const formMul =document.querySelector("#multiplicar-form");
+//const divMul= document.querySelector("#resultado-divMul");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumber = first.value;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const res= cd.calcularPara(firstNumber);
+
+  div.innerHTML = "<p>" +res +"</p>";
 });
 
-formMul.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const firstNumberMul = Number.parseInt(firstMul.value);
-  const secondNumberMul = Number.parseInt(secondMul.value);
-
-  divMul.innerHTML = "<p>" + multiplicar(firstNumberMul, secondNumberMul) + "</p>";
-});
+//formMul.addEventListener("submit", (event) => {
+//  event.preventDefault();
+//
+//  const firstNumberMul = Number.parseInt(firstMul.value);
+//  const secondNumberMul = Number.parseInt(secondMul.value);
+//
+//  divMul.innerHTML = "<p>" + multiplicar(firstNumberMul, secondNumberMul) + "</p>";
+//});
